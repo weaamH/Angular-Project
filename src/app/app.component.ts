433c56721aa08c4e815250d4575439de80818462
weaamH <weaamhijaji2001@gmail.com>
2022-08-16 22:25:29 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
+ interface userVM{
+  name: String;
+  user_name: String;
+  age: number;
+  email: String;
+  password: String;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Angular-Project';
+
+   public userArray: userVM[] = [];
+   public user: userVM={name: '', user_name: '', age: 0, email: '', password: ''};
+    
+
+   addUser(){
+    
+    this.userArray.push({...this.user});
+   }
 }
