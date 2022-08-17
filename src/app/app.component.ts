ffc3cd5b9f92105eb37886bb14fdb57b5f0a5d08
weaamH <weaamhijaji2001@gmail.com>
2022-08-17 12:20:55 +0300

    form validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
  interface userVM{
+  id: number;
   name: String;
-  user_name: String;
   age: number;
   email: String;
   password: String;
+  confirm_password: string;
 }
 @Component({
   selector: 'app-root',
@@ -16,11 +17,22 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
 
    public userArray: userVM[] = [];
-   public user: userVM={name: '', user_name: '', age: 0, email: '', password: ''};
-    
+   public user: userVM={id: 0, name: '', age: 0, email: '', password: '', confirm_password: ''};
 
-   addUser(){
-    
+
+   addUser(userForm: NgForm){
+    console.log(userForm.value);
+    console.log(userForm.valid);
+    if(userForm.valid){
+      this.user.id+=1;
+      console.log(userForm.value);
     this.userArray.push({...this.user});
+    }
+   }
+   deleteUser(user: userVM){
+    let index = this.userArray.indexOf(user);
+    if(index != -1){
+      this.userArray.splice(index, 1);
+    }
    }
 }
