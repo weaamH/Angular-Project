8d1442b9f0c500b49de72e33933080d478b41c92
weaamH <weaamhijaji2001@gmail.com>
2022-08-17 23:02:50 +0300

    karma.conf.js updated

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,7 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-
+const process = require('process');
+process.env.CHROME_BIN = require('puppeteer').executablePath();
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -39,9 +40,9 @@ module.exports = function (config) {
     autoWatch: false,
     captureTimeout: 60000,
     browserNoActivityTimeout: 360000,
-    browsers: ['Chrome', 'ChromeHeadless'],
-    customLaunchers: {
-      ChromeHeadlessNoSandbox: {
+    browsers: ['HeadlessChrome'],
+    customLaunchers:{
+      HeadlessChrome:{
         base: 'ChromeHeadless',
         flags: ['--no-sandbox']
       }
